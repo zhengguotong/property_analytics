41963a94033878475e04165d7eef28b00a0a05f0
Tony Zheng <tony@visionsoft.com.au>
2020-11-21 20:39:28 +1100

    fix github action sqllite action failed issue

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -17,13 +17,37 @@ class DatabaseSeeder extends Seeder
         Eloquent::unguard();
 
         //disable foreign key check for this connection before running seeders
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+       $this->setFKCheckOff();
         
         $this->call(PropertySeeder::class);
         $this->call(AnalyticTypeSeeder::class);
         $this->call(PropertyAnalyticSeeder::class);
 
         //restore foreign key check
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        $this->setFKCheckOn();
+    }
+
+    private function setFKCheckOff() 
+    {
+        switch(DB::getDriverName()) {
+            case 'mysql':
+                DB::statement('SET FOREIGN_KEY_CHECKS=0');
+                break;
+            case 'sqlite':
+                DB::statement('PRAGMA foreign_keys = OFF');
+                break;
+        }
+    }
+
+    private function setFKCheckOn() 
+    {
+        switch(DB::getDriverName()) {
+            case 'mysql':
+                DB::statement('SET FOREIGN_KEY_CHECKS=1');
+                break;
+            case 'sqlite':
+                DB::statement('PRAGMA foreign_keys = ON');
+                break;
+        }
     }
 }
